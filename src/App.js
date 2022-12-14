4b2aa95e39c7054d696ce8a8735da1692f8aae64
Yixuan WANG <wang.yixuan@live.fr>
2022-12-13 20:08:09 -0500

    Add Bulma

--- src/App.js
+++ src/App.js
@@ -3,7 +3,32 @@ import "./App.css";
 function App() {
   return (
     <div className='App'>
-      <h1>Hello world</h1>
+      <h1 class="title">
+        Bulma
+      </h1>
+
+      <p class="subtitle">
+        Modern CSS framework based on <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">Flexbox</a>
+      </p>
+
+      <div class="field">
+       
+      </div>
+
+      <div class="field">
+        <p class="control">
+          <span class="select">
+            <select>
+              <option>Select dropdown</option>
+            </select>
+          </span>
+        </p>
+      </div>
+
+      <div class="buttons">
+        <a class="button is-primary">Primary</a>
+        <a class="button is-link">Link</a>
+      </div>
     </div>
   );
 }
