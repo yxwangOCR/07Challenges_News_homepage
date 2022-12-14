81cf59bec227be74488f10dcb13f5101c658b8da
Yixuan WANG <wang.yixuan@live.fr>
2022-12-14 18:59:29 -0500

    Add desktop hero content layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <Image />
       <div className='hero-content'>
         <h1>The Bright Future of Web 3.0?</h1>
-        <div>
+        <div className='hero-text'>
           <p>
             We dive into the next evolution of the web that claims to put the
             power of the platforms back into the hands of the people. But is it
