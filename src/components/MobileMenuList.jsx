2ffaa2b2c807e24702526cd7b8fcb9b2a1f7bba3
Yixuan WANG <wang.yixuan@live.fr>
2022-12-14 23:49:58 -0500

    Add mobile menu list

---
+++ src/components/MobileMenuList.jsx
@@ -0,0 +1,17 @@
+import "../styles/Nav.css";
+
+const MobileMenuList = () => {
+  return (
+    <div className="modal">
+      <ul className='Mobile-menu-list'>
+        <a href='#'>Home</a>
+        <a href='#'>New</a>
+        <a href='#'>Popular</a>
+        <a href='#'>Trending</a>
+        <a href='#'>Categories</a>
+      </ul>
+    </div>
+  );
+};
+
+export default MobileMenuList;
--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,13 +1,32 @@
 import "../styles/Nav.css";
 import Logo from "../../src/assets/logo.svg";
 import MobileMenu from "../../src/assets/icon-menu.svg";
+import MobileMenuClose from "../../src/assets/icon-menu-close.svg";
+import MobileMenuList from "./MobileMenuList";
+import { useState } from "react";
 
 const MobileNav = () => {
+  const [open, setOpen] = useState(false);
+  const handleToggle = () => {
+    setOpen(!open);
+  };
   return (
-    <div className='mobile-navbar'>
-      <img src={Logo} alt='Website Logo' />
-      <img src={MobileMenu} alt='Mobile Nav menu' />
-    </div>
+    <>
+      <div className='mobile-navbar'>
+        <img src={Logo} alt='Website Logo' />
+
+        {open ? (
+          <img
+            src={MobileMenuClose}
+            alt='Mobile Nav menu'
+            onClick={handleToggle}
+          />
+        ) : (
+          <img src={MobileMenu} alt='Mobile Nav menu' onClick={handleToggle} />
+        )}
+      </div>
+      {open ? <MobileMenuList /> : null}
+    </>
   );
 };
 
