1c77f0e04cec294d18cc9df9e5f8e1a2f1285d34
Yixuan WANG <wang.yixuan@live.fr>
2022-12-14 21:22:00 -0500

    Add card style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,33 +9,28 @@ const Card = () => {
     <div className='card-container'>
       <div className='card'>
         <img src={img1} />
-        <div className='card-content'>
-          <h1>01</h1>
-          <Content
-            title='Reviving Retro PCs'
-            text='What happens when old PCs are given modern upgrades?'
-          />
-        </div>
+        <Content
+          number='01'
+          title='Reviving Retro PCs'
+          text='What happens when old PCs are given modern upgrades?'
+        />
       </div>
       <div className='card'>
         <img src={img2} />
-        <div className='card-content'>
-          <h1>02</h1>
-          <Content
-            title='Top 10 Laptops of 2022'
-            text='Our best picks for various needs and budgets.'
-          />
-        </div>
+
+        <Content
+          number='02'
+          title='Top 10 Laptops of 2022'
+          text='Our best picks for various needs and budgets.'
+        />
       </div>
       <div className='card'>
         <img src={img3} />
-        <div className='card-content'>
-          <h1>03</h1>
-          <Content
-            title='The Growth of Gaming'
-            text='How the pandemic has sparked fresh opportunities.'
-          />
-        </div>
+        <Content
+          number='03'
+          title='The Growth of Gaming'
+          text='How the pandemic has sparked fresh opportunities.'
+        />
       </div>
     </div>
   );
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,6 +1,7 @@
-const Content = ({ title, text }) => {
+const Content = ({ number, title, text }) => {
   return (
     <div className='content'>
+      <h1>{number}</h1>
       <h2>{title}</h2>
       <p>{text}</p>
     </div>
