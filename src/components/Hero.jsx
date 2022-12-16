ab6d35165828e2ece868f5fdcfde31e8b18a8aca
Yixuan WANG <wang.yixuan@live.fr>
2022-12-16 15:46:48 -0500

    Fix Hero responsive layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <Image />
       <div className='hero-content'>
         <h1>The Bright Future of Web 3.0?</h1>
-        <div className='hero-text'>
+        <div className='hero-items'>
           <p>
             We dive into the next evolution of the web that claims to put the
             power of the platforms back into the hands of the people. But is it
